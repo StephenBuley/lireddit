d27fdcf7676f5ee74be70428b363058eda1d4cd6
StephenBuley <65041377+StephenBuley@users.noreply.github.com>
2021-12-31 12:30:18 +0100

    GraphQL Setup
    Setup the basic infrastructure for graphql, including apollo server, express and graphql. Using graphql v15.3.0 to follow along more closely with the tutorial I am watching.

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,16 +1,31 @@
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants"
-import { Post } from "./entities/Post"
 import MikroOrmConfig from "./mikro-orm.config"
+import express from "express"
+import {ApolloServer} from "apollo-server-express"
+import {buildSchema} from "type-graphql"
+import { HelloResolver } from "./resolvers/hello"
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
 
 async function main() {
     const orm = await MikroORM.init(MikroOrmConfig)
     await orm.getMigrator().up()
-    // const post = orm.em.create(Post, {title: "My first Post"})
-    // await orm.em.persistAndFlush(post)
 
-    const posts = await orm.em.find(Post, {})
-    console.log(posts)
+    const app = express()
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        }),
+        plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+    })
+    await apolloServer.start()
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(4000, () => {
+        console.log("server started on localhost:4000")
+    })
 }
 
 main()
